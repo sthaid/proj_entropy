eb2c0a39f5e394fb14b6051645404310b98f77e6
Steven Haid <stevenhaid@gmail.com>
2015-03-15 10:41:05 -0400

    added android setup script

---
+++ android-project-setup/Entropy.java
@@ -0,0 +1,10 @@
+package org.sthaid.app;
+
+import org.libsdl.app.SDLActivity;
+
+/*
+ * A sample wrapper class that just calls SDLActivity
+ */
+
+public class Entropy extends SDLActivity { }
+
---
+++ android-project-setup/jni_src_Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+SDL_PATH := ../SDL2-2.0.3
+SDL_TTF_PATH := ../SDL2_ttf-2.0.12
+SDL_MIXER_PATH := ../SDL2_mixer-2.0.0
+
+LOCAL_CFLAGS += -DANDROID -fsigned-char
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
+                    $(LOCAL_PATH)/$(SDL_TTF_PATH) \
+                    $(LOCAL_PATH)/$(SDL_MIXER_PATH) 
+
+LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+                   entropy.c util.c
+
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf SDL2_mixer 
+
+include $(BUILD_SHARED_LIBRARY)
